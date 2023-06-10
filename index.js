8696e017fecb6e625d08e64402a9f931613e2466
escabora <gabrielescabora@gmail.com>
2023-06-10 16:31:34 -0300

    fix: ajust functioncreate cache

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-export default cacheApi = () => {
-  console.log('cache api');
-};
+const fse = require('fs-extra');
+
+async function cacheApi(pathOutput, nameOutPath, file) {
+  try {
+    const pathName = `./public/${pathOutput}/${nameOutPath}`;
+    fse.outputFileSync(pathName, JSON.stringify(file));
+    console.log('\x1b[32msuccess\x1b[0m', `create file in ${pathName}`);
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+module.exports = cacheApi;
