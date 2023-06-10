a620d6427caf15adcbbe7528115440236e7f1d2b
escabora <gabrielescabora@gmail.com>
2023-06-10 16:34:57 -0300

    chore: doc function create files

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const fse = require('fs-extra');
 
+/**
+ * @param {string} pathOutput - String referring to the file creation path
+ * @param {string} nameOutPath - String referring to the name and type of creation of the file
+ * @param {Object} file - API object
+ * @returns {Function} Returns asynchronous function to create the files
+ **/
 async function cacheApi(pathOutput, nameOutPath, file) {
   try {
     const pathName = `./public/${pathOutput}/${nameOutPath}`;
